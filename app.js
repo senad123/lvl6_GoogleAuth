4a7dfb6911e5bbe2377a124a101d51cebec21b4f
sbajra <senad.bajra@gmail.com>
2023-12-30 13:33:45 +0100

    revert user table

--- app.js
+++ app.js
@@ -103,10 +103,7 @@ passport.serializeUser((user, done) => {
 
 passport.deserializeUser(async (id, done) => {
   try {
-    const user = await db.oneOrNone(
-      "SELECT * FROM dbuser_bckp WHERE id = $1",
-      id
-    );
+    const user = await db.oneOrNone("SELECT * FROM users WHERE id = $1", id);
     return done(null, user);
   } catch (err) {
     return done(err);
